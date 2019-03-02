53055d7601e2196732fc4520d369a83479b9c698
Daniel Upshaw <contact@danielupshaw.com>
2019-03-02 01:09:36 -0500

    ...

--- index.js
+++ index.js
@@ -19,22 +19,25 @@ module.exports = function (filter) {
 		stylesheetKeys.forEach(function (key) {
 			// replace 兼容 min-width:768px 之类冒号后边没有空格的写法
 			var name = cleanUpString(key.replace(/:\s?/g, '-'));
+
 			if (typeof filter === 'function' && !filter(fileName, name)) return
 
-			var poop = file.clone({
+			var fileClone = file.clone({
 				contents: false
 			});
+
 			var contents = stringify(stylesheets[key]);
-			poop.contents = new Buffer(contents);
+			fileClone.contents = new Buffer(contents);
 
 			if (name) {
 				name = fileName + '-' + name
 				var filepath = pathjoin(dirname(file.path), name + '.css');
-				poop.path = filepath;
+				fileClone.path = filepath;
 			} else {
-				poop.path = pathjoin(dirname(file.path), fileName + '-lite.css');
+				fileClone.path = pathjoin(dirname(file.path), fileName + '-lite.css');
 			}
-			stream.push(poop);
+
+			stream.push(fileClone);
 		});
 		callback();
 	});
