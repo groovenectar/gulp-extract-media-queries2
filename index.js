cb099820451367f1a96ecd864d383a5388d4a38c
Daniel Upshaw <contact@danielupshaw.com>
2019-03-02 02:23:03 -0500

    Collect and save CSS for unfiltered files

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ var dirname = require('path').dirname;
 var pathjoin = require('path').join;
 var basename = require('path').basename;
 
+function indent(str, numOfIndents, opt_spacesPerIndent) {
+	str = str.replace(/^(?=.)/gm, new Array(numOfIndents + 1).join('\t'));
+	numOfIndents = new Array(opt_spacesPerIndent + 1 || 0).join(' '); // re-use
+	return opt_spacesPerIndent
+		? str.replace(/^\t+/g, function (tabs) {
+			return tabs.replace(/./g, numOfIndents);
+		})
+		: str;
+}
+
 module.exports = function (filter) {
 	return through.obj(function (cssFile, characterEncoding, callback) {
 		var stream = this;
@@ -15,29 +25,56 @@ module.exports = function (filter) {
 		var reworkData = rework(cssFile.contents.toString()).use(reworkMoveMedia());
 		var stylesheets = split(reworkData);
 		var stylesheetKeys = Object.keys(stylesheets);
+		var unfilteredStyles = [];
 
 		stylesheetKeys.forEach(function (mediaQuery) {
 			// Replace 兼容 min-width:768px 之类冒号后边没有空格的写法
 			var mediaQueryStringId = cleanUpString(mediaQuery.replace(/:\s?/g, '-'));
 
-			if (typeof filter === 'function' && !filter(fileName, mediaQueryStringId)) return
-
 			var fileClone = cssFile.clone({
 				contents: false
 			});
 
-			var contents = stringify(stylesheets[mediaQuery]);
-			fileClone.contents = new Buffer(contents);
+			var fileContents = stringify(stylesheets[mediaQuery]);
+			fileClone.contents = new Buffer(fileContents);
+
+			if (typeof filter === 'function' && !filter(fileName, mediaQueryStringId)) {
+				unfilteredStyles.push({mediaQuery: mediaQuery, css: stylesheets[mediaQuery]});
+				return;
+			}
 
 			if (mediaQueryStringId) {
 				mediaQueryStringId = fileName + '-' + mediaQueryStringId;
 				fileClone.path = pathjoin(dirname(cssFile.path), mediaQueryStringId + '.css');
 			} else {
-				fileClone.path = pathjoin(dirname(cssFile.path), fileName + '-lite.css');
+				fileClone.path = pathjoin(dirname(cssFile.path), fileName + '.css');
 			}
 
 			stream.push(fileClone);
 		});
+
+		if (unfilteredStyles.length) {
+			var fileClone = cssFile.clone({
+				fileContents: false
+			});
+
+			var fileContents = '';
+
+			unfilteredStyles.forEach(function (unfilteredStyle) {
+				if (unfilteredStyle.mediaQuery) {
+					fileContents += '@media ' + unfilteredStyle.mediaQuery + ' {' + "\n\t" +
+						indent(stringify(unfilteredStyle.css), 1) + "\n" +
+						'}' + "\n";
+				} else {
+					fileContents += stringify(unfilteredStyle.css);
+				}
+			});
+
+			fileClone.contents = new Buffer(fileContents);
+			fileClone.path = pathjoin(dirname(cssFile.path), fileName + '.css');
+
+			stream.push(fileClone);
+		}
 
 		callback();
 	});
